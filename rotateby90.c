0487c8f2bd733c8299c12c372b19167a4fe4a7d7
sahilattri2 <sahilgithub6@gmail.com>
2025-01-11 00:36:02 +0530

    added matrix rotation  by 90 degree

---
+++ rotateby90.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#define MAX 100
+
+void readMatrix(int matrix[MAX][MAX], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+}
+
+void transposeMatrix(int matrix[MAX][MAX], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+}
+
+void reverseRows(int matrix[MAX][MAX], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n / 2; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[i][n - j - 1];
+            matrix[i][n - j - 1] = temp;
+        }
+    }
+}
+
+void printMatrix(int matrix[MAX][MAX], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n;
+    int matrix[MAX][MAX];
+    printf("Enter dimension of matrix:");
+    scanf("%d", &n);
+
+    readMatrix(matrix, n);
+    transposeMatrix(matrix, n);
+    reverseRows(matrix, n);
+    printMatrix(matrix, n);
+
+    return 0;
+}

d706fc647e284b153b22651d200b8390e14b03f5
sahilattri2 <sahilgithub6@gmail.com>
2025-01-30 10:10:17 +0530

    Update Search in a Circular Sorted Linked List.c

--- Search in a Circular Sorted Linked List.c
+++ Search in a Circular Sorted Linked List.c
@@ -1,67 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+ 
 struct Node {
     int data;
     struct Node* next;
 };
-
+ 
 struct Node* newNode(int data) {
     struct Node* node = (struct Node*)malloc(sizeof(struct Node));
     node->data = data;
     node->next = NULL;
     return node;
 }
-
+ 
+struct Node* findMiddle(struct Node* start, struct Node* end) {
+    if (start == NULL) return NULL;
+    struct Node* slow = start;
+    struct Node* fast = start;
+ 
+    while (fast != end && fast->next != end) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+ 
 int binarySearch(struct Node* head, int key) {
+    if (head == NULL) return -1;
+ 
     struct Node* start = head;
-    struct Node* end = head;
-
-    if (head == NULL) {
-        return -1;
-    }
-
-    while (end->next != start) {
-        end = end->next;
-    }
-
-    while (start != end) {
-        struct Node* mid = start;
-        while (mid != end && mid->next != end) {
-            mid = mid->next;
-        }
-
-        if (mid->data == key) {
+    struct Node* end = NULL;
+ 
+    do {
+        struct Node* mid = findMiddle(start, end);
+        if (mid == NULL)
+            return -1;
+        if (mid->data == key)
             return 1;
-        } else if (mid->data > key) {
+        if (mid->data > key) {
             end = mid;
         } else {
             start = mid->next;
         }
-    }
-
+    } while (end != start);
+ 
     return -1;
 }
-
+ 
 void printList(struct Node* head) {
-    struct Node* temp = head;
     if (head == NULL) return;
+    struct Node* temp = head;
     do {
         printf("%d -> ", temp->data);
         temp = temp->next;
     } while (temp != head);
-    printf("\n");
+    printf("(Head)\n");
 }
-
+ 
 int main() {
     struct Node* head = NULL;
     struct Node* temp;
     int n, value, key;
-    
+ 
     printf("Enter the number of nodes: ");
     scanf("%d", &n);
-    
-    printf("Enter the elements of the list:\n");
+ 
+    printf("Enter the elements of the sorted list:\n");
     for (int i = 0; i < n; i++) {
         scanf("%d", &value);
         if (head == NULL) {
@@ -73,16 +77,20 @@ int main() {
         }
     }
     temp->next = head;
-
+ 
+    printf("The Circular Linked List is: \n");
+    printList(head);
+ 
     printf("Enter the key to search: ");
     scanf("%d", &key);
-
+ 
     int result = binarySearch(head, key);
     if (result != -1) {
-        printf("Position: %d\n", result);
+        printf("Key found in the list.\n");
     } else {
-        printf("Key not found\n");
+        printf("Key not found.\n");
     }
-
+ 
     return 0;
 }
+has context menu
